0c68e72c88073063b12a61352df0e453d455767e
khmerdao <58039862+khmerdao@users.noreply.github.com>
2019-12-12 12:07:44 +0100

    Gestion des differents bugs
    Il ne manque plus qu'a rendre tout ce bazar joli

--- index.js
+++ index.js
@@ -8,17 +8,23 @@ class List{
 //#### FIN CLASS ####
 
 //#### DEBUT DATA ####
-if(localStorage.length == 0) { //insere une liste vide dans le localStorage si il est vide
+console.log("la key = "+localStorage.getItem("liste 1")); // creer par defaut une key "liste 1" dans le local storage
+if(localStorage.length == 0) { //insere une liste vide par defaut dans le localStorage si il est vide
+    localStorage.setItem("liste 1", "[]"); 
+}
+else if(localStorage.getItem("liste 1") == null){
     localStorage.setItem("liste 1", "[]"); 
 }
 
+let key1 = localStorage.getItem("liste 1");
 let listAll1 = []; //creation d'un listing
-let key1 = localStorage.getItem("liste 1"); // creer par defaut une key "liste 1" dans le local storage
 let listeAafficher = JSON.parse(key1); //transforme ce qu'il y a dans le localStorage en JSON
 
 let ajouter = document.getElementById("ajouter");
 let formulaire = document.getElementById("formulaire");
 let listing = document.getElementById("liste1");
+
+ajouter.className = "red";
 
 let nbrLigneMax = 5; //compteur de nbr de ligne
 let nbrSlide = 1; // nbr de slide par defaut
@@ -42,37 +48,6 @@ let nbrSlide = 1; // nbr de slide par defaut
 //     }
 // }
 
-// function afficheCarousel(tabs) //Nouvel fonction affiche pour le carousel 
-// {
-
-//     let liste0 = document.getElementById("liste0");
-//     // let liste1 = document.getElementById("liste1");
-//     // let liste2 = document.getElementById("liste2");
-//     let liste = liste0;
-//     for(let j=0; j<nbrSlide; j++)
-//     {
-//         liste = document.getElementById(`liste${j}`);
-//         let limite = nbrLigneMax;
-//         for(let i=0; i<tabs.length; i++){
-        
-//             if(i == limite){
-//                 liste = liste1;
-//                 limite += 5;
-//             }
-//             else if(i == 10){
-//                 liste = liste2;
-//             }
-//             liste.innerHTML += `
-//             <div id="ligne${i}" class="ligne">
-//                 <input type="checkbox"  id="check${i}" class="check" onclick="checker(this, ${i})">
-//                 <label class="txt" for="check${i}">${tabs[i].text}</label>
-//                 <button class="supprimer" type="submit" form="formulaire" onclick="del(this, ${i})">X</button>
-//             </div>`
-//         }
-//     }
-
-// }
-
 function afficheCarousel(tabs) //Nouvel fonction affiche pour le carousel 
 {
     let limite = 0;
@@ -82,12 +57,22 @@ function afficheCarousel(tabs) //Nouvel fonction affiche pour le carousel
         let liste = document.getElementById(`liste${j}`);
         console.log(liste);
         for(let i=limite; i<limite+nbrLigneMax; i++){ // 5 lignes
-            liste.innerHTML += `
-            <div id="ligne${i}" class="ligne">
-                <input type="checkbox"  id="check${i}" class="check" onclick="checker(this, ${i})">
-                <label class="txt" for="check${i}">${tabs[i].text}</label>
-                <button class="supprimer" type="submit" form="formulaire" onclick="del(this, ${i})">X</button>
-            </div>`;
+            if(tabs[i].done == true){ //verif si la tache est faite oup pas
+                liste.innerHTML += `
+                <div id="ligne${i}" class="ligne">
+                    <input type="checkbox"  id="check${i}" class="check" onclick="checker(this, ${i})" checked>
+                    <label class="txt" for="check${i}">${tabs[i].text}</label>
+                    <button class="supprimer" type="submit" form="formulaire" onclick="del(this, ${i})">X</button>
+                </div>`;
+            }
+            else{
+                liste.innerHTML += `
+                <div id="ligne${i}" class="ligne">
+                    <input type="checkbox"  id="check${i}" class="check" onclick="checker(this, ${i})">
+                    <label class="txt" for="check${i}">${tabs[i].text}</label>
+                    <button class="supprimer" type="submit" form="formulaire" onclick="del(this, ${i})">X</button>
+                </div>`;
+            }
         }
         limite = limite + nbrLigneMax;
     }
@@ -96,6 +81,7 @@ function afficheCarousel(tabs) //Nouvel fonction affiche pour le carousel
 function checker(e, i){ //CHange la valeur de "".done"
     // console.log(e.checked);
     listAll1[i].done = e.checked;
+    localStorage.setItem("liste 1", JSON.stringify(listAll1));  
     // console.log(listAll1[i].done);
 }
 
@@ -140,7 +126,7 @@ function ajoutSlide(nbrSlide){
     //ajout le l'icone rectangulaire du slide
     let barreSlide = document.querySelector(".carousel-indicators");
     barreSlide.innerHTML +=  `
-        <li data-target="#carouselToDo" data-slide-to="${nbrSlide-1}"></li>
+        <li data-target="#carouselToDo" data-slide-to="${nbrSlide}"></li>
     `;
 }
 
